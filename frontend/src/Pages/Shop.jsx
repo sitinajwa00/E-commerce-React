1d8706e7caff5274672470f96cf5030d35f72c56
sitinajwa00 <s.najwapandi@gmail.com>
2024-09-01 23:50:37 +0800

    Add Component
    Hero, Item, New Collection, Offers, News Letter, Popular

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Hero } from '../Components/Hero/Hero'
+import { NewCollections } from '../Components/NewCollections/NewCollections'
+import { NewsLetter } from '../Components/NewsLetter/NewsLetter'
+import { Offers } from '../Components/Offers/Offers'
+import { Popular } from '../Components/Popular/Popular'
 
 export const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+        <Hero />
+        <Popular />
+        <Offers />
+        <NewCollections />
+        <NewsLetter />
+    </div>
   )
 }
