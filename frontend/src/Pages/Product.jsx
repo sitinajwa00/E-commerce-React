668d7635a2b59506082a27e2af45e8823b73e3a4
sitinajwa00 <s.najwapandi@gmail.com>
2024-09-03 12:19:12 +0800

    Add Breadcrum

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom';
+import { Breadcrum } from '../Components/Breadcrums/Breadcrum';
+import { ShopContext } from '../Context/ShopContext'
 
 export const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId));
   return (
-    <div>Product</div>
+    <div>
+      <div>
+        <Breadcrum product = {product} />
+      </div>
+    </div>
   )
 }
